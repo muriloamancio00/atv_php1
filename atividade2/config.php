948426ea0218e022d3a88e32abe98305240481e5
murilo <61051754+muriloamancio00@users.noreply.github.com>
2022-06-08 09:01:38 -0300

    quase completo
    feito em sala

---
+++ atividade2/config.php
@@ -0,0 +1,18 @@
+<?php
+
+    class config {
+
+        // Nome do DB
+        public const DB_NOME = 'tads20_murilo';
+        // Usuário do DB
+        public const DB_USUARIO = 'tads20_murilo';
+        // Senha do DB
+        public const DB_SENHA = 'cavalomanco';
+        // Charset da conexão PDO
+        public const DB_CHARSET = 'utf8';
+    }
+
+
+    //
+
+    
